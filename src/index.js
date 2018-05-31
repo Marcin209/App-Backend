5db89100397f2a15ddbf6ce9e2f08a38471d2283
msobczak <msobczak@it-dev.pl>
2018-05-31 10:21:12 +0200

    Book model

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-var express = require('express');
+var express = require('express'),
+    mongose = require('mongoose');
+
+var db = mongose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('../models/bookModel');
 
 var app = express();
 var port = process.env.PORT || 3000;
+
+var bookRouter = express.Router();
+bookRouter.route('/Books')
+    .get(function (req,res) {
+        // var respond = "HEllo THER FELLO KIDS";
+        // res.json(respond);
+        Book.find(function (err,books) {
+            if(err){
+                console.log("Wystapil blad");
+            }else {
+                res.json(books);
+            }
+        })
+    });
+app.use('/api',bookRouter);
 
 app.get('/', function(req, res) {
     res.send("some Test ")
