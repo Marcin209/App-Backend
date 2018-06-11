56e7ead1cf96e684ef83e2376d3e415113d6cc85
msobczak <msobczak@it-dev.pl>
2018-06-11 09:34:22 +0200

    changing from mongose to mysql

--- src/index.js
+++ src/index.js
@@ -1,32 +1,46 @@
-var express = require('express'),
-    mongose = require('mongoose');
-
-var db = mongose.connect('mongodb://localhost/bookAPI');
-
-var Book = require('../models/bookModel');
+var express = require('express');
+var mysql = require('mysql');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-var bookRouter = express.Router();
-bookRouter.route('/Books')
-    .get(function (req,res) {
-        // var respond = "HEllo THER FELLO KIDS";
-        // res.json(respond);
-        Book.find(function (err,books) {
-            if(err){
-                console.log("Wystapil blad");
-            }else {
-                res.json(books);
-            }
-        })
-    });
-app.use('/api',bookRouter);
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port:'3306',
+    user: 'root',
+    password: 'qwerty1234',
+    database: 'It_apps'
+});
 
-app.get('/', function(req, res) {
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('connected as id ' + connection.threadId);
+});
+
+// connection.on('error',function(err){
+//    console.log(err.message);
+// });
+
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//     if (error) throw error;
+//     console.log('The solution is: ', results[0].solution);
+// });
+
+connection.end();
+
+var Router = express.Router();
+Router.route('/restApi')
+    .get(function (req, res) {
+        console.log('Dziala Rooter')
+    });
+app.get('/', function (req, res) {
     res.send("some Test ")
 });
 
-app.listen(port,function () {
+app.listen(port, function () {
     console.log("Listening on Port 3000")
 });
